69b02fbb139b796d4813066061a9b7c2919daffe
Lucas Lima <lucas.lima.psico@hotmail.com>
2023-10-08 21:18:45 -0300

    feat: reset project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import './globals.css'
-import Providers from './provider/themeProvider'
 
 export const metadata = {
     title: 'Oi, sou o Lucas Lima',
@@ -10,9 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="pt-br" suppressHydrationWarning>
             <>
-                <body>
-                    <Providers>{children}</Providers>
-                </body>
+                <body>{children}</body>
             </>
         </html>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,9 @@
-import About from './elements/about'
-import Techs from './elements/techs'
-import Works from './elements/works'
-import Project from './elements/myprojects'
-import Menu from './elements/menu'
 import { inter } from './styles/fonts'
-import Footer from './elements/footer'
 
 export default function Home() {
     return (
         <div className={inter.className}>
-            <div className="h-90">
-                <Menu />
-                <About />
-                <Techs />
-                <Works />
-                <Project />
-                <Footer />
-            </div>
+            <div className="h-90"></div>
         </div>
     )
 }
